c0b4df0b6ccb954ebda5914550a2844f3d0ca3fb
ian root <ianaroot@gmail.com>
2014-03-27 15:17:51 -0700

    add robot image to animation, bot finds number of paths when allowed to walk in any direction but not retrace steps

--- robot_recursion.js
+++ robot_recursion.js
@@ -1,69 +1,99 @@
-var Robot = {
-  initialize: function(){
-    this.x = 0,
-    this.y = 0
-    this.delay = 0
-    return this
-  },
-  checkPosition: function(x, y, board){
+
+function Robot(){
+
+  this.x = 0,
+  this.y = 0,
+  this.delay = 0
+
+  this.checkPosition = function(x, y, board, cellHistory){
+    var cellHistory
+    if (!cellHistory) {
+      cellHistory = ''
+    }
     x = x,
     y = y
-    // console.log(board)
-    console.log(x)
-    console.log(y)
-    console.log()
-    if (x === board.goal[0] && y === board.goal[1]) {
-      console.log("found the goal!")
-      this.animate(x,y, "blue")
-      this.animate(x,y, "white")
-      return 1
-    }
-    else if (board[y] && board[y][x]) {
-      console.log("still lookin")
-      this.animate(x,y, "lightBlue")
-      var valueFromRecursions =  (this.checkPosition(x +1, y, board) + this.checkPosition(x, y + 1, board))
-      this.animate(x,y,"white")
-      return valueFromRecursions
+    if (cellHistory.indexOf("[" + y + "," + x + "]") == -1) {
+      cellHistory += "[" + y + "," + x + "]"
+      if (x === board.goal[0] && y === board.goal[1]) {
+        
+        this.animate(x,y, "blue")
+        this.animate(x,y, "white")
+        return 1
       }
+      else if (board[y] && board[y][x]) {
+        
+        this.animate(x,y, "lightBlue")
+        var valueFromRecursions =  (this.checkPosition(x +1, y, board, cellHistory) + this.checkPosition(x, y + 1, board, cellHistory) + this.checkPosition(x - 1, y, board, cellHistory) + this.checkPosition(x, y - 1, board, cellHistory))
+        this.animate(x,y,"white")
+        return valueFromRecursions
+        }
+      else {
+        
+        return 0
+      }
+    }
     else {
-      console.log("hit the wall")  
       return 0
     }
+
   },
-  numberOfPathsToGoal: function(board){
-    return this.checkPosition(0, 0, board)
+  this.numberOfPathsToGoal = function(board){
+    var pathNum = this.checkPosition(0, 0, board)
   },
-  animate: function(x,y, color){
+  this.animate = function(x,y, color){
     // if (board[y] && board[y][x]) {
       var cell = document.getElementById(y + ',' + x)
 
       // var cell = document.getElementById('2,2')
-      console.log(cell)
+      
       // cell.style.backgroundColor="blue" 
-      this.delay += 400
-      setTimeout(setColor, this.delay, cell, color)
+      this.delay += 130
+      setTimeout(setBackground, this.delay, cell, color)
     // }
   }
 }
 
-var Board = {
+
+Board = {
   initialize: function(width, height){
-    var board = new Array();
+    this.matrix = new Array();
     for (i = 0; i < height; i ++) {
-      board[i] = new Array();
+      this.matrix[i] = new Array();
       for (j = 0; j < width; j ++) {
-        board[i][j] = 1
+        this.matrix[i][j] = 1
       }
     }
-    board.goal = [width - 1, height - 1]
-    return board
+    this.matrix.goal = [width - 1, height - 1]
+    return this
+  },
+  draw: function(){
+    
+  },
+  cellHtml: '<div class="cell" id="0,0"></div>',
+
+  rowHtml: '<div class="row"></div>'
+}
+
+setBackground = function(element, color){
+  if (color == "blue") {
+    var results = document.getElementById('answer')
+    var currentTotal = parseInt(results.innerHTML) + 1
+    element.style.backgroundColor = "red"
+    results.innerHTML = currentTotal
+  }
+  else if (color == "white") {
+    element.style.backgroundColor = "white"
+    element.style.backgroundImage = ""
+  }
+  else if (color == "lightBlue") {
+    element.style.backgroundImage= 'url(7_Bradshaw1.jpg)'
   }
 }
 
-function setColor(element, color){
-  element.style.backgroundColor=color
-}
 
 var board = Board.initialize(5,3)
+var bot = new Robot
 
-var bot = Robot.initialize()
+window.onload = function(){
+  bot.numberOfPathsToGoal(board.matrix)  
+}
--- robot_recursion.rb
+++ robot_recursion.rb
@@ -9,25 +9,36 @@ BOARD = [
 
 def find_paths(x, y, goal)
   moves = []
-  return check(x, y, goal, 1, moves)
+  return check(x, y, goal, 1, moves, "")
 end
 
-def check(x, y, goal, i, axis)
+def check(x, y, goal, i, axis, cell_history)
+  cell_history = cell_history
   puts
+  puts "cell_history is #{cell_history}"
   puts "last move was on the #{axis} axis"
   puts "i is #{i}"
   i += 1
   puts "#{x}, #{y}"
-  if [x, y] == goal
+  # if !cell_history.include?([#{y},#{x}])
+  if cell_history.include?("#{y},#{x}") || x < 0 || y < 0
+    return 0
+  elsif [x, y] == goal
+    cell_history += "[#{y},#{x}]"
     puts "goal"
     1
   elsif BOARD[x] && BOARD[x][y]
+    cell_history += "[#{y},#{x}]"
     puts "still looking"
-    return check(x+1, y, goal, i, "x") + check(x, y + 1, goal, i, "y")
+    return check(x+1, y, goal, i, "x", cell_history) + check(x, y + 1, goal, i, "y", cell_history) + check(x - 1, y, goal, i, "x", cell_history) + check(x, y - 1, goal, i, "y", cell_history)
   else
+    cell_history += "[#{y},#{x}]"
     puts "wall"
     0
   end
+  # else
+  #   return 0
+  # end
 end
 
 p find_paths(0,0, [(BOARD.length - 1),(BOARD[1].length - 1)])
